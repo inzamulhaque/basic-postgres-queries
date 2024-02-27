fefe1936bc63a2722dcbfcb455c2ba9033c5bdb3
inzamulhaque <mdihalif@gmail.com>
2024-02-28 01:00:13 +0600

    use constraint and truncate

--- alter.sql
+++ alter.sql
@@ -3,19 +3,39 @@
 SELECT * FROM person2;
 
 ALTER TABLE person2
-    ADD COLUMN name VARCHAR(25) DEFAULT 'Default Name' NOT NULL;
+ADD COLUMN name VARCHAR(25) DEFAULT 'Default Name' NOT NULL;
+
+ALTER TABLE person2 DROP COLUMN name;
+
+ALTER TABLE person2 RENAME COLUMN age TO user_age;
+
+ALTER TABLE person2 ALTER COLUMN user_name TYPE VARCHAR(50);
+
+ALTER TABLE person2 ALTER COLUMN user_age SET NOT NULL;
+
+ALTER TABLE person2 ALTER COLUMN user_age DROP NOT NULL;
 
 ALTER TABLE person2
-    DROP COLUMN name;
+ADD constraint unique_person2_user_age UNIQUE (user_age);
 
-ALTER TABLE person2
-    RENAME COLUMN age TO user_age;
+ALTER TABLE person2 DROP constraint unique_person2_user_age;
 
-ALTER TABLE person2
-    ALTER COLUMN user_name TYPE VARCHAR(50);
+ALTER TABLE person2 DROP constraint unique_person2_user_age;
 
-ALTER TABLE person2
-    ALTER COLUMN user_age SET NOT NULL;
+ALTER TABLE person2 ADD constraint pk_person2_id PRIMARY KEY (id);
 
-ALTER TABLE person2
-    ALTER COLUMN user_age DROP NOT NULL;
+ALTER TABLE person2 DROP constraint pk_person2_id;
+
+TRUNCATE TABLE person2;
+
+INSERT INTO
+    person2 (user_name, email, user_age)
+VALUES (
+        'user1', 'user1@email.com', 19
+    ),
+    (
+        'user2', 'user2@email.com', 20
+    ),
+    (
+        'user3', 'user3@email.com', 21
+    )
